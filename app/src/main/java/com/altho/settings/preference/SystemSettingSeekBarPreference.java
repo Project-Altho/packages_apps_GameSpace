ac0d64193f20535cd15be5facd2322baf49d71eb
Dinesh Kumar <dseera6@gmail.com>
2022-07-03 06:11:25 +0530

    GameSpace: Import Bringup from EvolutionOS
    - Additionally keeping original Copyrights of EvoX
    
    Signed-off-by: Dinesh Kumar <dseera6@gmail.com>

--- app/src/main/java/com/altho/settings/preference/SystemSettingSeekBarPreference.java
+++ app/src/main/java/com/altho/settings/preference/SystemSettingSeekBarPreference.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2019-2022 Evolution X
+ * Copyright (C) 2022 Altho
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.evolution.settings.preference;
+package com.altho.settings.preference;
 
 import android.content.Context;
 import android.util.AttributeSet;
